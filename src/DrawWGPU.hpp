58f357949279fceb2de16da2e008c0191940213c
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-08-02 19:19:44 +0200

    Scale Intensity depending on type

--- src/DrawWGPU.hpp
+++ src/DrawWGPU.hpp
@@ -75,7 +75,7 @@ void UpdateGui(wgpu::RenderPassEncoder renderPass, ES::Engine::Core &core) {
 		}
 		ImGui::ColorEdit4("Color", glm::value_ptr(lights[i].color));
 		ImGui::DragFloat3("Direction(Directional)/Position(Point)", glm::value_ptr(lights[i].direction), 0.1f);
-		ImGui::DragFloat("Intensity", &lights[i].intensity, 10.f);
+		ImGui::DragFloat("Intensity", &lights[i].intensity, (lights[i].type == Light::Type::Directional ? 0.1f : 10.f));
 		ImGui::Combo("Type", (int *)&lights[i].type, "Directional\0Point\0Spot\0");
 		ImGui::PopID();
 	}
