68a1fb1dc43928831f0cf18326ba5fbe7df6c9bd
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2025-08-04 15:28:04 +0200

    Add default lights back

--- src/DrawWGPU.hpp
+++ src/DrawWGPU.hpp
@@ -7,6 +7,7 @@
 #include <imgui.h>
 #include <backends/imgui_impl_wgpu.h>
 #include <backends/imgui_impl_glfw.h>
+#include "UpdateLights.hpp"
 
 
 void UpdateGui(wgpu::RenderPassEncoder renderPass, ES::Engine::Core &core) {
@@ -82,41 +83,7 @@ void UpdateGui(wgpu::RenderPassEncoder renderPass, ES::Engine::Core &core) {
 	ImGui::EndChild();
 
 	if (lightsDirty) {
-		auto &device = core.GetResource<wgpu::Device>();
-		auto &pipelineData = core.GetResource<Pipelines>().renderPipelines["3D"];
-		auto &bindGroups = core.GetResource<BindGroups>();
-		auto &queue = core.GetResource<wgpu::Queue>();
-
-		lightsBuffer.destroy();
-		lightsBuffer.release();
-
-		wgpu::BufferDescriptor lightsBufferDesc(wgpu::Default);
-		lightsBufferDesc.size = sizeof(Light) * std::max(lights.size(), size_t(1)) + sizeof(uint32_t) + 12 /* (padding) */;
-		lightsBufferDesc.usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Storage;
-		lightsBufferDesc.label = wgpu::StringView("Lights Buffer");
-		lightsBuffer = device.createBuffer(lightsBufferDesc);
-
-		uint32_t lightsCount = static_cast<uint32_t>(lights.size());
-		queue.writeBuffer(lightsBuffer, 0, &lightsCount, sizeof(uint32_t));
-		queue.writeBuffer(lightsBuffer, sizeof(uint32_t), lights.data(), sizeof(Light) * lights.size());
-
-		bindGroups.groups["2"].release();
-
-		wgpu::BindGroupEntry bindingLights(wgpu::Default);
-		bindingLights.binding = 0;
-		bindingLights.buffer = lightsBuffer;
-		bindingLights.size = sizeof(Light) * std::max(lights.size(), size_t(1)) + sizeof(uint32_t) + 12 /* (padding) */; // TODO: Resize when adding a new light
-
-		std::array<wgpu::BindGroupEntry, 1> lightsBindings = { bindingLights };
-
-		wgpu::BindGroupDescriptor bindGroupLightsDesc(wgpu::Default);
-		bindGroupLightsDesc.layout = pipelineData.bindGroupLayouts[1];
-		bindGroupLightsDesc.entryCount = lightsBindings.size();
-		bindGroupLightsDesc.entries = lightsBindings.data();
-		bindGroupLightsDesc.label = wgpu::StringView("Lights Bind Group");
-		auto bg = device.createBindGroup(bindGroupLightsDesc);
-
-		bindGroups.groups["2"] = bg;
+		UpdateLights(core);
 	}
 
 	ImGui::End();
